2422ca788f961b521c7f37742c74f82c503142c9
Sidnei Farias <sidneijr.77@gmail.com>
2024-02-20 12:13:18 -0300

    feature: add the rest of the basic UI

---
+++ src/components/Module.tsx
@@ -0,0 +1,37 @@
+import * as Collapsible from '@radix-ui/react-collapsible';
+
+import { ChevronDown } from "lucide-react";
+import { Lesson } from "./Lesson";
+
+interface ModuleProps {
+  moduleIndex: number;
+  title: string;
+  amountOfLessons: number;
+}
+
+export function Module({ moduleIndex, title, amountOfLessons }: Readonly<ModuleProps>) {
+  return (
+    <Collapsible.Root className='group'>
+      <Collapsible.Trigger className="flex w-full items-center gap-3 bg-zinc-800 p-4">
+        <div className="flex h-10 w-10 rounded-full items-center justify-center bg-zinc-950 text-xs">
+          {moduleIndex + 1}
+        </div>
+
+        <div className="flex flex-col gap-1 text-left">
+          <strong className="text-sm">{title}</strong>
+          <span className="text-xs text-zinc-400">{amountOfLessons} aulas</span>
+        </div>
+
+        <ChevronDown className="w-5 h-5 ml-auto text-zinc-400 group-data-[state=open]:rotate-180 transition-transform" />
+      </Collapsible.Trigger>
+
+      <Collapsible.Content>
+        <nav className="relative flex flex-col gap-4 p-6">
+          <Lesson title="Fundamentos do Redux" duration="09:13" />
+          <Lesson title="Fundamentos do Redux" duration="09:13" />
+          <Lesson title="Fundamentos do Redux" duration="09:13" />
+        </nav>
+      </Collapsible.Content>
+    </Collapsible.Root>
+  );
+}
