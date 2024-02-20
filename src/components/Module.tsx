82ed98f81426fe7b9a2a0f66237e602f58d4ede9
Sidnei Farias <sidneijr.77@gmail.com>
2024-02-20 18:06:58 -0300

    feat: highlight current lesson

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -15,6 +15,12 @@ interface ModuleProps {
 export function Module({ moduleIndex, title, amountOfLessons }: Readonly<ModuleProps>) {
   const dispatch = useDispatch()
 
+  const { currentModuleIndex, currentLessonIndex } = useAppSelector((state) => {
+    const { currentModuleIndex, currentLessonIndex } = state.player
+
+    return { currentModuleIndex, currentLessonIndex }
+  })  
+
   const lessons = useAppSelector((state) => {
     return state.player.course.modules[moduleIndex].lessons
   })
@@ -37,12 +43,15 @@ export function Module({ moduleIndex, title, amountOfLessons }: Readonly<ModuleP
       <Collapsible.Content>
         <nav className="relative flex flex-col gap-4 p-6">
           {lessons.map((lesson, lessonIndex) => {
+            const isCurrent = currentModuleIndex === moduleIndex && currentLessonIndex === lessonIndex
+
             return (
               <Lesson
                 key={lesson.id}
                 title={lesson.title}
                 duration={lesson.duration}
                 onPlay={() => dispatch(play([moduleIndex, lessonIndex]))}
+                isCurrent={isCurrent}
               />
             )
           })}
